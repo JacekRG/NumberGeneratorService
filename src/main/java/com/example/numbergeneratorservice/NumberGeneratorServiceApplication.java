606eb5cb9ca4f81bcc8dd5492450483933767c76
Jacek <steiner@10g.pl>
2023-02-02 16:56:41 +0100

    git init commit

---
+++ src/main/java/com/example/numbergeneratorservice/NumberGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.numbergeneratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberGeneratorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/numbergeneratorservice/NumberGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numbergeneratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberGeneratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
