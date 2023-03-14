85541d33077428d5cda3df3847977d9d7932f084
Jacek <steiner@10g.pl>
2023-03-14 14:33:37 +0100

    NumberGeneratorService Connection to fix

--- src/test/java/pl/lotto/infrastructure/numbersgenerator/http/NumbersReceiverClientImplTest.java
+++ src/test/java/pl/lotto/infrastructure/numbersgenerator/http/NumbersReceiverClientImplTest.java
@@ -1,50 +1,94 @@
 package pl.lotto.infrastructure.numbersgenerator.http;
 
-import org.junit.jupiter.api.Test;
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static wiremock.org.hamcrest.MatcherAssert.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
-class NumbersReceiverClientImplTest {
+@Import(ConfigProperties.class)
+@EnableConfigurationProperties(value = ConfigProperties.class)
+@ContextConfiguration(classes = ConfigProperties.class)
+@TestPropertySource("classpath:application-properties.yml")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(SpringExtension.class)
+public class NumbersReceiverClientImplTest {
 
-    @Mock
-    private RestTemplate restTemplate;
+    RestTemplate restTemplate;
+    ResponseEntity response;
+    WireMockServer wireMockServer;
 
-    @Test
-    void shouldReturnDrawDateWhenRestTemplateReturnsOkStatus() {
-        // given
-        NumbersReceiverClientImpl client = new NumbersReceiverClientImpl(restTemplate);
-        LocalDateTime exemplaryDate = LocalDateTime.parse("2022-12-24T12:00:00");
-        DrawDateDto expectedDrawDateDto = new DrawDateDto(exemplaryDate);
-        ResponseEntity<DrawDateDto> response = new ResponseEntity<>(expectedDrawDateDto, HttpStatus.OK);
-        given(restTemplate.getForEntity(any(String.class), eq(DrawDateDto.class))).willReturn(response);
+    @Autowired
+    private ConfigProperties configProperties;
 
-        // when
-        DrawDateDto actualDrawDateDto = client.specifyDrawDate();
-
-        // then
-        assertEquals(expectedDrawDateDto, actualDrawDateDto);
+    @BeforeEach
+    public void setup() {
+        restTemplate = new RestTemplate();
+        response = null;
+        wireMockServer = new WireMockServer(wireMockConfig().port(configProperties.getPort()));
+        wireMockServer.start();
+wireMockServer.resetAll();
     }
-//
+
 //    @Test
-//    void shouldThrowExceptionWhenRestTemplateReturnsNotFoundStatus() {
-//        // given
-//        NumbersReceiverClientImpl client = new NumbersReceiverClientImpl(restTemplate);
-//        given(restTemplate.getForEntity(any(String.class), eq(DrawDateDto.class))).willThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
+//    @Order(2)
+//    public void givenWireMockAdminEndpoint_whenGetWithoutParams_thenVerifyRequest() {
 //
+//        RestTemplate restTemplate = new RestTemplate();
 //
+//        response = restTemplate.getForEntity(createHttpLink()+"/", String.class);
+//
+//        assertThat("Verify Response Body", response.getBody().toString().contains("mappings"));
+//        assertThat("Verify Status Code", response.getStatusCode().equals(HttpStatus.OK));
+//        wireMockServer.stop();
 //    }
 
+
+//    @Test
+//    @Order(1)
+//    public void testResourceApi() {
+//        System.out.println(configProperties.getHost());
+//        wireMockServer.stubFor(
+//                get(WireMock.urlEqualTo("/"))
+//                        .willReturn(aResponse()
+//                                .withStatus(HttpStatus.OK.value())
+//                                .withHeader("Content-Type", TEXT_PLAIN_VALUE)
+//                                .withBody("test resource")));
+//
+//        response = restTemplate.getForEntity(createHttpLink(), String.class);
+//
+//        assertThat("Verify Response Body", response.getBody().toString().contains("test resource"));
+//        assertThat("Verify Status Code", response.getStatusCode().equals(HttpStatus.OK));
+//
+//        wireMockServer.stop();
+//    }
+
+    private String createHttpLink() {
+        StringBuilder httpLink = new StringBuilder();
+        httpLink.append("http://");
+        httpLink.append(configProperties.getHost());
+        httpLink.append(":");
+        httpLink.append(configProperties.getPort());
+        httpLink.append("");
+        return httpLink.toString();
+    }
 }
---
+++ src/test/java/pl/lotto/infrastructure/numbersgenerator/http/SimplyNumbersReceiverClientImplTest.java
@@ -0,0 +1,49 @@
+package pl.lotto.infrastructure.numbersgenerator.http;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SimplyNumbersReceiverClientImplTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Test
+    void shouldReturnDrawDateWhenRestTemplateReturnsOkStatus() {
+        // given
+        NumbersReceiverClientImpl client = new NumbersReceiverClientImpl(restTemplate);
+        LocalDateTime exemplaryDate = LocalDateTime.parse("2022-12-24T12:00:00");
+        DrawDateDto expectedDrawDateDto = new DrawDateDto(exemplaryDate);
+        ResponseEntity<DrawDateDto> response = new ResponseEntity<>(expectedDrawDateDto, HttpStatus.OK);
+        given(restTemplate.getForEntity(any(String.class), eq(DrawDateDto.class))).willReturn(response);
+
+        // when
+        DrawDateDto actualDrawDateDto = client.specifyDrawDate();
+
+        // then
+        assertEquals(expectedDrawDateDto, actualDrawDateDto);
+    }
+//
+//    @Test
+//    void shouldThrowExceptionWhenRestTemplateReturnsNotFoundStatus() {
+//        // given
+//        NumbersReceiverClientImpl client = new NumbersReceiverClientImpl(restTemplate);
+//        given(restTemplate.getForEntity(any(String.class), eq(DrawDateDto.class))).willThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
+//
+//
+//    }
+
+}
